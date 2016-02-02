97f986a4dec9f15650a2ee1ae38253095fbf3621
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2016-02-02 20:05:48 +0100

    Web UI: integrate Lightsquid into server-manager

---
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Lightsquid.php
@@ -0,0 +1,6 @@
+<?php
+
+$L['Lightsquid_Description'] = 'Web proxy statistics';
+$L['Lightsquid_Tags'] = 'statistics stats analysis graph squid web proxy';
+
+$L['Lightsquid_Title'] = 'Proxy statistics';
--- root/usr/share/nethesis/NethServer/Module/Lightsquid.php
+++ root/usr/share/nethesis/NethServer/Module/Lightsquid.php
@@ -1,44 +1,47 @@
 <?php
-namespace NethServer\Module\Dashboard\Applications;
+
+namespace NethServer\Module;
 
 /*
- * Copyright (C) 2013 Nethesis S.r.l.
- *
+ * Copyright (C) 2011 Nethesis S.r.l.
+ * 
  * This script is part of NethServer.
- *
+ * 
  * NethServer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * NethServer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with NethServer.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+use Nethgui\System\PlatformInterface as Validate;
+
 /**
- * Lightsquid web interface
+ * CGP integration
  *
- * @author Giacomo Sanchietti
+ * @author Giacomo Sanchietti<giacomo.sanchietti@nethesis.it>
  */
-class Lightsquid extends \Nethgui\Module\AbstractModule implements \NethServer\Module\Dashboard\Interfaces\ApplicationInterface
+class Lightsquid extends \Nethgui\Controller\AbstractController
 {
 
-    public function getName()
+    protected function initializeAttributes(\Nethgui\Module\ModuleAttributesInterface $base)
     {
-        return "Lightsquid";
+        return \Nethgui\Module\SimpleModuleAttributesProvider::extendModuleAttributes($base, 'Status');
     }
 
-    public function getInfo() 
+
+    public function prepareView(\Nethgui\View\ViewInterface $view)
     {
+        parent::prepareView($view);
         $cweb = $this->getPlatform()->getDatabase('configuration')->getKey('lightsquid');
         $host = explode(':',$_SERVER['HTTP_HOST']);
-        return array(
-            'url' => "/".$cweb['alias']
-        );
-    } 
+        $view['url'] = "/".$cweb['alias'];
+    }
 }
